fdad5737a095fad53fa5e1a64447a7ea1bc7d2ff
tanviriqbal <tanviriqbal4u@live.com>
2018-05-02 11:07:41 +0600

    TimeSpan

--- Program.cs
+++ Program.cs
@@ -6,9 +6,11 @@ namespace DateTimeAndTimeSpan
     {
         static void Main(string[] args)
         {
-            DateTimeExample();
+            //DateTimeExample();
+            TimeSpanExample();
         }
 
+        // Represent Exact Date Time
         public static void DateTimeExample()
         {
             DateTime dateTime = new DateTime(1989,3,28,10,30,45);
@@ -26,6 +28,33 @@ namespace DateTimeAndTimeSpan
             System.Console.WriteLine(dateTime.ToLongTimeString()); // 10:30:45 AM
             System.Console.WriteLine(dateTime.ToShortTimeString()); // 10:30 AM
             System.Console.WriteLine(dateTime.ToString("dd MMM yyyy")); // 28 Mar 1989
+        }
+
+        public static void TimeSpanExample()
+        {
+            // Creating
+            TimeSpan timeSpan = new TimeSpan(1,2,3); // hour, minute, second
+            System.Console.WriteLine(timeSpan); // 01:02:03
+
+            DateTime dateTime1 = new DateTime(1989,3,28,10,30,45);
+            DateTime dateTime2 = new DateTime(1989,3,28,10,45,45);
+            TimeSpan diffDateTime = dateTime2 - dateTime1;
+            System.Console.WriteLine(diffDateTime); // 00:15:00
+
+            // Properties
+            System.Console.WriteLine(timeSpan.Hours); // 1
+            System.Console.WriteLine(timeSpan.Minutes); // 2
+            System.Console.WriteLine(timeSpan.Seconds); // 3
+            System.Console.WriteLine(timeSpan.TotalHours); // 1.03416666666667
+            System.Console.WriteLine(timeSpan.TotalMinutes); // 62.05
+            System.Console.WriteLine(timeSpan.TotalSeconds); // 3723
+
+            // Add, Subtract
+            System.Console.WriteLine(timeSpan.Add(TimeSpan.FromMinutes(8))); // 01:10:03
+            System.Console.WriteLine(timeSpan.Subtract(TimeSpan.FromMinutes(2))); // 01:00:03
+
+            // Parse
+            System.Console.WriteLine(TimeSpan.Parse("02:30:50")); //02:30:50
         }
     }
 }
