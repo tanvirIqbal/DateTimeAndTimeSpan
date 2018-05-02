98adf3922d54cbdd4c68cbb7b2857d465aa27564
tanviriqbal <tanviriqbal4u@live.com>
2018-05-02 09:36:41 +0600

    Create Console Project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DateTimeAndTimeSpan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
